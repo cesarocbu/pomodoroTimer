a0899578f9833684b4e05fd6c7f12eadecd9ce40
Cesar Ochoa <cesar_8ab@hotmail.com>
2023-05-05 23:54:58 -0600

    v1.0

---
+++ main.js
@@ -0,0 +1,54 @@
+let workP = document.getElementById('work')
+let breakP = document.getElementById('short-break')
+
+let workTime = 1
+let breakTime = 5
+let seconds = "00"
+
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime
+    document.getElementById('seconds').innerHTML = seconds
+    workP.classList.add('active')
+}
+
+//Start count
+
+function startCount() {
+    document.getElementById('start').style.display = "none"
+    document.getElementById('reset').style.display = "block"
+
+
+    seconds = 59
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0
+
+    let timerFunction = () => {
+        document.getElementById('minutes').innerHTML = workMinutes
+        document.getElementById('seconds').innerHTML = seconds
+        
+        seconds = seconds - 1
+        if(seconds === 0){
+            workMinutes = workMinutes - 1
+            if(workMinutes === -1){
+                if(breakCount % 2 === 0){
+                    workMinutes = breakMinutes
+                    breakCount++
+
+                    workP.classList.remove('active')
+                    breakP.classList.add('active')
+                } else{
+                    workMinutes = workTime
+                    breakCount++
+
+                    breakP.classList.remove('active')
+                    workP.classList.add('active')
+                }
+            }
+            seconds = 59;
+        }
+    }
+
+    setInterval(timerFunction, 100)
+}
